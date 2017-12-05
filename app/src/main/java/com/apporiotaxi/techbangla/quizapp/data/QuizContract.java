b689ec9da353249605c0a68b58b0c68ff4f6e670
emonm <belalmd016@gmail.com>
2017-12-05 15:45:04 +0600

    Initial commit

---
+++ app/src/main/java/com/apporiotaxi/techbangla/quizapp/data/QuizContract.java
@@ -0,0 +1,25 @@
+package com.apporiotaxi.techbangla.quizapp.data;
+
+/**
+ * Created by emon on 11/30/2017.
+ */
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by delaroy on 11/30/17.
+ */
+
+public class QuizContract {
+
+    public static class MovieEntry implements BaseColumns {
+        public static final String TABLE_QUEST = "quest";
+        // tasks Table Columns names
+        public static final String KEY_ID = "id";
+        public static final String KEY_QUES = "question";
+        public static final String KEY_ANSWER = "answer"; //correct option
+        public static final String KEY_OPTA= "opta"; //option a
+        public static final String KEY_OPTB= "optb"; //option b
+        public static final String KEY_OPTC= "optc"; //option c
+    }
+}
